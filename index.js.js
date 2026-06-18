61425d6c6e5c43c3e67bc13abb2acf3963654d45
Boriimo <114356451+Boriimo@users.noreply.github.com>
2026-06-18 15:29:28 +0100

    Refactor Telegram and WhatsApp integration code

--- index.js.js
+++ index.js.js
@@ -86,4 +86,8 @@ waClient.on('ready', () => {
     }, new NewMessage({}));
 
     waClient.initialize();
+    waClient.on('authenticated', (session) => {
+    console.log("إليك مفتاح واتساب الطويل (انسخه):");
+    console.log(JSON.stringify(session));
+});
 })();
